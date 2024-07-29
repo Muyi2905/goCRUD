da4ad4a0967a5b8a4109cd0adcc61daacbcc68c9
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-29 15:28:59 +0100

    update

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoded/json"
 	"fmt"
 	"log"
-	"net/http"
-	"encoded/json"
-	"strconv"
 	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct{
@@ -23,9 +25,23 @@ type Director struct{
 
 var movies[]Movie
 
+func getmovies(w http.ResponseWriter, r*http.Request){
+	if err:= r.URL.Path!= "GET"; err!=nil{
+http.Error(w, "error", http.StatusNotFound)
+	}
+}
+
 func main(){
 movies = append(movies, Movie{Id: "1", Title: "Interstallar", Director: &Director{firstName:"Christopher", lastName: "Nolan"}})
 movies= append(movies, Movie{Id: "2", Title: "Dune2", Director: &Director{firstName: "Denis", lastName: "Villenuve"} })
 
+r:= mux.NewRouter()
+r.HandleFunc("/movies",getmovies).Methods("GET")
+r.HandleFunc("/movies{id}", getmovie).Methods("GET")
+r.HandleFunc("/movies",createmovie).Methods("POST")
+r.HandleFunc("/movies{id}", updatemovie).Methods("PATCH")
+r.Handle("/movies{id}", deletemovie).Methods("DELETE")
 
+fmt.Println("server is running on port 8080")
+log.Fatal(http.ListenAndServe(";8080", r))
 }
